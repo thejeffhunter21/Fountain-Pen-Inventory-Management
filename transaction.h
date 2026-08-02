c51a507e21c4f1531a0e01168afa8c5b6717f99c
thejeffhunter21 <vincent.s.nguyen87@gmail.com>
2026-08-01 22:55:14 -0700

    yeah

---
+++ transaction.h
@@ -0,0 +1,16 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include <string>
+#include <vector>
+#include "inventory.h"   // needed since TransactionLog methods take an Inventory&
+
+// your enum class TransactionType goes here
+
+// your Transaction class goes here
+
+// your PriceChangeTransaction : public Transaction (if you go that route) goes here
+
+// your TransactionLog class goes here
+
+#endif
